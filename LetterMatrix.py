dd9dfd6a17397fa3d5556b414d7b8230dfd0adf9
joachim <joachim.kueppers@gmx.net>
2019-12-16 17:32:20 +0100

    Added test functions for LetterMatrix

--- LetterMatrix.py
+++ LetterMatrix.py
@@ -1,14 +1,14 @@
 class LetterMatrix:
     def __init__(self, inputMatrix):
         self.inputRows = inputMatrix.splitlines()
+        self.rowLength = len(self.inputRows[0])
+        self.columnLength = len(self.inputRows)
 
     def getNumberOfInputRows(self):
         return len(self.inputRows)
 
     def correctInput(self):
-        rowLength = len(self.inputRows[0])
         for row in self.inputRows:
-            if len(row) != rowLength:
+            if len(row) != self.rowLength:
                 return False
         return True
-
